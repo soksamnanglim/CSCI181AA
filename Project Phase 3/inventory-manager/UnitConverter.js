4958c6259d56e70e4e4555b37579cceaa4ed5572
Stephanie Yu <syaa2018@mymail.pomona.edu>
2021-03-27 20:51:11 -0700

    Added unit converter.

---
+++ Project Phase 3/inventory-manager/UnitConverter.js
@@ -0,0 +1,56 @@
+/**
+ * The Unit Converter assists the Inventory Manager by converting raw groceries
+ * to std units (GroceryItem(s) to StandardGroceryItem(s)).
+ */
+class UnitConverter { 
+ 
+    /**
+     * Solid conversions to the standard solid unit (kg).
+     * <from-unit> : <multiplyByFactor>
+     */
+    static solidConversions = [
+        {lb : 0.454}, // To-do: populate the array with all solid conversions. 
+    ];
+
+    /**
+     * Liquid conversions to the standard liquid unit (L).
+     * <from-unit> : <multiplyByFactor>
+     */
+    static liquidConversions = [
+        {qt : 0.946}, // To-do: populate the array with all liquid conversions. 
+    ];
+
+    /**
+     * Converts GroceryItems to std units (kg for solids, L for liquids). Calls convertSolidUnit
+     * for each solid grocery and convertLiquidUnit for each liquid grocery.
+     * 
+     * @param {GroceryItem[]} groceryItems - A GroceryItem array. 
+     * @returns {StandardGroceryItem[]} - A StandardGroceryItem array. 
+     */
+    standardizeUnits(groceryItems){
+    
+    }
+
+    /**
+     * Converts a solid grocery to std unit (kg) via a multiplication factor
+     * lookup in the solidConversion array. 
+     * 
+     * @param {GroceryItem} groceryItem - a solid GroceryItem.
+     * @returns {StandardGroceryItem} - a solid StandardGroceryItem. 
+     */
+    convertSolidUnit(groceryItem){
+
+    }
+
+    /**
+     * Converts a liquid grocery to std unit (L) via a multiplication factor
+     * lookup in the liquidConversion array. 
+     * 
+     * @param {GroceryItem} groceryItem - a liquid GroceryItem.
+     * @returns {StandardGroceryItem} - a liquid StandardGroceryItem.  
+     */
+    convertLiquidUnit(grocery){
+
+    }
+
+}
