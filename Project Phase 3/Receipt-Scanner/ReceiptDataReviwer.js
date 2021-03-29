3e37961117fa2e68b78844961125fc074ad0880a
Kenneth Ochieng <47263196+Kennethochiyeng@users.noreply.github.com>
2021-03-28 22:54:10 -0700

    Create ReceiptDataReviwer.js

---
+++ Project Phase 3/Receipt-Scanner/ReceiptDataReviwer.js
@@ -0,0 +1,41 @@
+/*
+
+This class will be useful in the review and editing of the JSON data
+from ReceiptDataExtractor. It will link to the UI to save any data changes
+and update item/object properties.
+
+I have interests to continue researching more on it in future
+release versions and plan a sort of learning element 
+so that the user never needs to edit the data.
+
+*/
+
+
+/* 
+//extracted receipt data as a Json file
+JsonObject receiptData 
+
+*/
+
+class ReceiptDataReviewer {}
+/* 
+the constructor will instantiate the receiptData variable
+*/
+
+
+itemIterator(receiptData);
+
+/* 
+this method will iterate through every object in the receiptData 
+and populate a textbox for the user for each object
+
+a listener will track a save action by user and we'll update 
+the current object. 
+*** I am still considering a lot more on how this method will precisely work best. 
+
+*/
+
+
+
+
+
