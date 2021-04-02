0bc14b2c4b700fc8e07f30413a27fe1d0d2ab14e
Shifa Somji <shifamsomji@gmail.com>
2021-04-01 22:56:52 -0700

    design for receipt.js

--- Project Phase 3/user-interactive/receipt.js
+++ Project Phase 3/user-interactive/receipt.js
@@ -4,6 +4,14 @@ class receipt {
     uploadReceipt(upload = true) {
         /*
             ask the user to upload a receipt
+            save the receipt
+            use the receipt to call the OCR component to process it
+            various tutorials for uploading images in react native:
+            - https://heartbeat.fritz.ai/how-to-upload-images-in-a-react-native-app-4cca03ded855
+            - https://tech-blog.maddyzone.com/reactnative/how-to-upload-image-in-react-native-app
+
+            documentation: https://www.npmjs.com/package/react-native-photo-upload
+
             @param upload: bool variable indicating whether user has clicked "upload receipt" button
             @output receiptFilePath: string variable for receipt's file path
         */
@@ -12,8 +20,14 @@ class receipt {
     verifyReceipt(data) {
         /*
             ask the user to verify receipt ingredients from OCR
-            @param data: a json object of user's receipt input with correct name/quantity/units
-            @output correctData: an updated json object of user verified ingredients
+            input will look like ["targe eggs", "safew milk"] 
+            the user will see these ingredients on the screen
+            - will be able to modify the ingredients
+            - for example: change "targe eggs" to "eggs" or "target eggs"
+            updated ingredients will be saved as a new list
+
+            @param data: a list of user's receipt input with correct name/quantity/units
+            @output correctData: an updated list of user verified ingredients
         */
     }
 }
