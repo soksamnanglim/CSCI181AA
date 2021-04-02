a8db6411875ea5b9fa374c79fec3a7366c417d27
Shifa Somji <shifamsomji@gmail.com>
2021-04-01 22:05:51 -0700

    design for preferences.js

--- Project Phase 3/user-interactive/preferences.js
+++ Project Phase 3/user-interactive/preferences.js
@@ -1,10 +1,15 @@
-// This module creates the preferences data object
+// This module''s responsbility is to create the preferences data object
+// It will ask the user for their dietary restrictions and ingredient preferences
 
 class preferences {
-    userPreferences(preferences = true) {
+    userPreferences(choosePreferences = true) {
         /*
             ask the user which ingredients they prefer to use for the recipe
-            @param preferences: bool variable indicating whether user has clicked "choose preferences" button
+            store ingredients in a list
+            list will be compiled with other inputs in createPreferencesObject()
+
+            @param choosePreferences: bool variable indicating whether user has clicked "choose preferences" button
+            @output preferences: list of user's ingredient preferences
         */
     }
 
@@ -12,6 +17,9 @@ class preferences {
         /*
             ask the user their dietary restrictions 
             only asked once during set up of account
+            store dietary restrictions in a list 
+            list will be compiled with other inputs in createPreferencesObject()
+
             @param chooseDietary: bool variable indicating whether user has clicked "set dietary restrictions" button
             @output dietary: list of user's dietary restrictions
         */
@@ -20,6 +28,14 @@ class preferences {
     createPreferencesObject(preferences, dietary, inventory) {
         /*
             create the preferences data object
+            - this object will be of the following format: 
+            - [["coconut", "chicken"], ["pork"], ["chicken", "cheese", "milk"]]
+            - the first subarray contains user preferences
+            - the second subarray contains dietary restrictions
+            - the third subarray contains all ingredients in inventory
+            create a query call to spoonacular api 
+            input to query call will be the preferences object
+
             @param preferences: user's preferences as a list
             @param dietary: user's dietary restrictions as a list
             @param inventory: current ingredients in user's inventory as a list
